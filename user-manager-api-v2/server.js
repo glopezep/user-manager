6b56337e7abb468c2088586977f09f6e72f4f269
xDstriker <xdstriker@Guillermo.local>
2017-09-13 16:07:51 -0400

    example with express added

--- user-manager-api-v2/server.js
+++ user-manager-api-v2/server.js
@@ -1,20 +1,17 @@
-const { microGraphiql, microGraphql } = require("graphql-server-micro")
-const micro = require("micro")
-const { get, post, router } = require("microrouter")
+const express = require('express')
+const bodyParser = require('body-parser')
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
 const chalk = require('chalk')
 const schema = require("./schema")
 
+const app = express();
 const port = process.env.PORT || 3000
 
-const server = micro(
-  router(
-    get("/graphql", microGraphql({ schema })),
-    post("/graphql", microGraphql({ schema })),
-    get("/graphiql", microGraphiql({ endpointURL: "/graphql" })),
-    (req, res) => micro.send(res, 404, "not found")
-  )
-)
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql'
+}))
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`${chalk.green('[user-manager-api]')} Server listening on port ${port}`)
 })
