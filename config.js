b55e4d0481c3797f63501f57ce33405efd6e5767
xDstriker <xdstriker@Guillermo.local>
2017-09-15 11:45:32 -0400

    dialect changed

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || '',
     host: process.env.DB_HOST || 'localhost',
-    dialect: process.env.DB_DIALECT || 'mysql',
+    dialect: process.env.DB_DIALECT || 'postgres',
     pool: {
       max: 10,
       min: 0,
